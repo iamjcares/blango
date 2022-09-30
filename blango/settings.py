a15f546474193f58c9ca25343139ce2b318536ac
Olalekan Johnson <johnnycares4toda@gmail.com>
2022-09-30 22:26:03 +0000

    Finish simplejwt

--- blango/settings.py
+++ blango/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
 from pathlib import Path
-
+from datetime import timedelta
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -68,6 +68,7 @@ REST_FRAMEWORK = {
         "rest_framework.authentication.BasicAuthentication",
         "rest_framework.authentication.SessionAuthentication",
         "rest_framework.authentication.TokenAuthentication",
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
     ],
     "DEFAULT_PERMISSION_CLASSES": [
         # "rest_framework.permissions.IsAuthenticated",
@@ -91,6 +92,11 @@ REST_FRAMEWORK = {
             "django_filters.rest_framework.DjangoFilterBackend",
             "rest_framework.filters.OrderingFilter"
     ],
+}
+
+SIMPLE_JWT = {
+        "ACCESS_TOKEN_LIFETIME": timedelta(days=1),
+        "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
 }
 
 ROOT_URLCONF = 'blango.urls'
