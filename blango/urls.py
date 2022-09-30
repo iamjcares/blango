ac142d0f0261584f2a55b04a3a9b8bc57693a364
Olalekan Johnson <johnnycares4toda@gmail.com>
2022-09-30 13:29:55 +0000

    Finish django registration

--- blango/urls.py
+++ blango/urls.py
@@ -16,11 +16,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 import blog.views
+import blango_auth.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", blog.views.index),
     path("post/<slug>/", blog.views.post_detail, name="blog-post-detail"),
     # path("api/v1/", include("blog.api_urls")),
+    path("accounts/", include("django.contrib.auth.urls")),
+    path("accounts/profile/", blango_auth.views.profile, name="profile"),
     path("api/v1/", include("blog.api.urls"))
 ]
