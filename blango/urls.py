e0fbb51644faf1f09b47b35656d512220f06d08f
Olalekan Johnson <johnnycares4toda@gmail.com>
2022-09-30 10:48:30 +0000

    Finish DRF views

--- blango/urls.py
+++ blango/urls.py
@@ -21,5 +21,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("", blog.views.index),
     path("post/<slug>/", blog.views.post_detail, name="blog-post-detail"),
-    path("api/v1/", include("blog.api_urls"))
+    # path("api/v1/", include("blog.api_urls")),
+    path("api/v1/", include("blog.api.urls"))
 ]
