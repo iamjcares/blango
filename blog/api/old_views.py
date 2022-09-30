e10c81b0e5b7b6133032089ca21000602fda1d9e
Olalekan Johnson <johnnycares4toda@gmail.com>
2022-09-30 21:06:10 +0000

    Finish filtering

--- blog/api/old_views.py
+++ blog/api/old_views.py
@@ -5,11 +5,6 @@ from blog.models import Post, Tag
 from blango_auth.models import User
 from blog.api.permissions import AuthorModifyOrReadOnly, IsAdminUserForObject
 
-from django.db.models import Q
-from django.utils import timezone
-from datetime import timedelta
-from django.http import Http404
-
 from rest_framework.decorators import action
 from rest_framework.response import Response
 
@@ -27,42 +22,6 @@ class PostDetail(generics.RetrieveUpdateDestroyAPIView):
 class PostViewSet(viewsets.ModelViewSet):
     permission_classes = [AuthorModifyOrReadOnly | IsAdminUserForObject]
     queryset = Post.objects.all()
-
-    def get_queryset(self):
-        if self.request.user.is_anonymous:
-            # published only
-            queryset = self.queryset.filter(published_at__lte=timezone.now())
-
-        elif not self.request.user.is_staff:
-            # allow all
-            queryset = self.queryset
-        else:
-            queryset = self.queryset.filter(
-                Q(published_at__lte=timezone.now()) | Q(author=self.request.user)
-            )
-
-        time_period_name = self.kwargs.get("period_name")
-
-        if not time_period_name:
-            # no further filtering required
-            return queryset
-
-        if time_period_name == "new":
-            return queryset.filter(
-                published_at__gte=timezone.now() - timedelta(hours=1)
-            )
-        elif time_period_name == "today":
-            return queryset.filter(
-                published_at__date=timezone.now().date(),
-            )
-        elif time_period_name == "week":
-            return queryset.filter(published_at__gte=timezone.now() - timedelta(days=7))
-        else:
-            raise Http404(
-                f"Time period {time_period_name} is not valid, should be "
-                f"'new', 'today' or 'week'"
-            )
-
 
     def get_serializer_class(self):
         if self.action in ("list", "create"):
